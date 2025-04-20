d3aa839766a00ba8c446775062c306c4860feb1f
ingabireol <ingabire069@gmail.com>
2025-04-20 19:05:55 +0200

    creating a documentation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,11 @@ module com.olim.employeemanagementsystem {
             requires org.kordamp.ikonli.javafx;
             requires org.kordamp.bootstrapfx.core;
             requires eu.hansolo.tilesfx;
-        
+
     opens com.olim.employeemanagementsystem to javafx.fxml;
+    opens com.olim.employeemanagementsystem.view to javafx.fxml;
+    opens com.olim.employeemanagementsystem.model to javafx.base;
+
     exports com.olim.employeemanagementsystem;
+    exports com.olim.employeemanagementsystem.view;
 }
